1ad1780b148ad90f28273c2819126c012fe64c47
prog <evprog@gmail.com>
2022-07-31 09:22:49 +0500

    поиск + произвольная папка сканирования + описание

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -8,12 +8,18 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
+    uslovie = ""
+    if request.method == 'POST':
+        poisk = request.form.get('poisk',"")
+        for u in poisk.split():
+            uslovie += " AND file_path LIKE '%" + u + "%'"
+        
     conn = get_db_connection()
-    rows = conn.execute('SELECT * FROM files').fetchall()
+    rows = conn.execute('SELECT * FROM files WHERE 1' + uslovie).fetchall()
     conn.close()
-    return render_template('index.html',rows=rows)
+    return render_template('index.html',uslovie=uslovie,rows=rows)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=5000)
